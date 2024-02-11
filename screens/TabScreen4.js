c04643bdfea245db6f7e838ef2a20aed28ce0977
yeeunli <127955610+yeeunli@users.noreply.github.com>
2024-02-11 23:15:28 +0900

    채팅탭 서버 연결 중

--- screens/TabScreen4.js
+++ screens/TabScreen4.js
@@ -6,6 +6,7 @@ import { createAppContainer } from "react-navigation";
 import { createStackNavigator } from "react-navigation-stack";
 import { FlatList, TouchableOpacity } from "react-native-gesture-handler";
 import { GiftedChat, Bubble } from "react-native-gifted-chat";
+import { idToken } from "./LoginScreen";
 
 const styles = StyleSheet.create({
   container: {
@@ -46,13 +47,50 @@ const TabHeader = ({ name }) => (
 const ChatListScreen = ({ navigation }) => {
   const [chatRooms, setChatRooms] = React.useState([]);
 
+  const createChatRoom = async (inviteUserEmail) => {
+    try {
+      const response = await fetch("http://pumasi.everdu.com/chat/", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `${idToken}`,
+        },
+        body: JSON.stringify({ invite_user: inviteUserEmail }),
+      });
+
+      if (response.status === 201) {
+        const responseData = await response.json();
+        const roomId = responseData.room_id;
+      } else {
+        console.error("Failed to create chat room:", response.status);
+      }
+    } catch (error) {
+      console.error("Error creating chat room:", error.message);
+    }
+  };
+
+  const fetchChatRooms = async () => {
+    try {
+      const response = await fetch("http://pumasi.everdu.com/chat/list", {
+        method: "GET",
+        headers: {
+          // 인증 헤더
+        },
+      });
+
+      if (!response.ok) {
+        throw new Error("채팅방을 불러오는 데 실패했습니다");
+      }
+
+      const fetchedChatRooms = await response.json();
+      setChatRooms(fetchedChatRooms);
+    } catch (error) {
+      console.error("채팅방을 불러오는 중 오류 발생:", error.message);
+    }
+  };
+
   React.useEffect(() => {
-    // mock data... 나중에 서버랑 연결할거임
-    const fetchedChatRooms = [
-      { id: 1, name: "서준맘" },
-      { id: 2, name: "세은대디" },
-    ];
-    setChatRooms(fetchedChatRooms);
+    fetchChatRooms();
   }, []);
 
   const navigateToChatRoom = (roomId, roomName) => {
@@ -79,44 +117,49 @@ const ChatListScreen = ({ navigation }) => {
 
 const ChatRoomScreen = ({ navigation }) => {
   const [messages, setMessages] = React.useState([]);
+  const roomId = navigation.getParam("roomId", 0);
+
+  const fetchMessages = async () => {
+    try {
+      const response = await fetch(`http://pumasi.everdu.com/chat/${roomId}`, {
+        method: "GET",
+        headers: {
+          // 인증 헤더
+        },
+      });
+
+      if (!response.ok) {
+        throw new Error("메시지를 불러오는 데 실패했습니다");
+      }
+
+      const fetchedMessages = await response.json();
+      setMessages(fetchedMessages);
+    } catch (error) {
+      console.error("메시지를 불러오는 중 오류 발생:", error.message);
+    }
+  };
 
   React.useEffect(() => {
-    // mock data...
-    const roomId = navigation.getParam("roomId", 0);
-    let fetchedMessages = [];
-
-    if (roomId === 1) {
-      // 서준맘과의 채팅
-      fetchedMessages = [
-        {
-          _id: 1,
-          text: "안녕하세요!",
-          createdAt: new Date(),
-          user: {
-            _id: 2,
-            name: "서준맘",
-          },
-        },
-      ];
-    } else if (roomId === 2) {
-      // 세은대디와의 채팅
-      fetchedMessages = [
-        {
-          _id: 1,
-          text: "좀이따 뵙겠습니다.",
-          createdAt: new Date(),
-          user: {
-            _id: 2,
-            name: "세은대디",
-          },
-        },
-      ];
-    }
-    setMessages(fetchedMessages);
+    fetchMessages();
   }, []);
 
-  const onSend = (newMessages = []) => {
-    setMessages((prevMessages) => GiftedChat.append(prevMessages, newMessages));
+  const onSend = async (newMessages = []) => {
+    try {
+      await fetch(`http://pumasi.everdu.com/chat/${roomId}/message`, {
+        method: "POST",
+        headers: {
+          // 인증 헤더
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ message: newMessages[0].text }),
+      });
+
+      setMessages((prevMessages) =>
+        GiftedChat.append(prevMessages, newMessages)
+      );
+    } catch (error) {
+      console.error("메시지를 전송하는 중 오류 발생:", error.message);
+    }
   };
 
   return (
@@ -132,18 +175,18 @@ const ChatRoomScreen = ({ navigation }) => {
             {...props}
             textStyle={{
               right: {
-                color: "#ffffff", // 오른쪽(나의 메시지) 텍스트 색상
+                color: "#ffffff", // 내 텍스트색
               },
               left: {
-                color: "#616161", // 왼쪽(상대방의 메시지) 텍스트 색상
+                color: "#616161", // 상대 텍스트색
               },
             }}
             wrapperStyle={{
               left: {
-                backgroundColor: "#A5D699", // 왼쪽(상대방의 메시지) 배경색
+                backgroundColor: "#A5D699", // 상대 배경색
               },
               right: {
-                backgroundColor: "#D9D9D9", // 오른쪽(나의 메시지) 배경색
+                backgroundColor: "#D9D9D9", // 내 배경색
               },
             }}
           />
@@ -163,9 +206,9 @@ const AppNavigator = createStackNavigator(
     },
     ChatRoom: {
       screen: ChatRoomScreen,
-      navigationOptions: {
-        headerShown: false,
-      },
+      navigationOptions: ({ navigation }) => ({
+        title: navigation.getParam("roomName", "채팅방"),
+      }),
     },
   },
   {
